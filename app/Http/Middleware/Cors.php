9c949ec9211babf48595e64a99b8c70ae8cbc0bd
maksudbhuiyan595 <mdmaksudbhuiyan595@gmail.com>
2024-09-08 16:46:44 +0600

    cors policy update

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -8,11 +8,21 @@ use Symfony\Component\HttpFoundation\Response;
 
 class Cors
 {
+
     public function handle(Request $request, Closure $next): Response
     {
-        return $next($request)
-        ->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'Content-Type, Authorizations');
+        $response = $next($request);
+
+        $response->headers->set('Access-Control-Allow-Origin', 'http://192.168.10.211:3000');
+        $response->headers->set('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
+
+        // Handle preflight requests
+        if ($request->getMethod() === 'OPTIONS') {
+            $response->headers->set('Access-Control-Max-Age', '3600');
+            $response->headers->set('Access-Control-Allow-Credentials', 'true');
+            return $response;
+        }
+        return $response;
     }
 }
