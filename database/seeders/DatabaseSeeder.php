29750098bee87e07306e480d1cc0fe59d14c9e59
maksudbhuiyan595 <mdmaksudbhuiyan595@gmail.com>
2024-09-15 17:56:27 +0600

    credentials

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,20 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        // User::factory()->create([
-        //     'first_name' => 'Abdur',
-        //     'last_name'=> 'Rahman',
-        //     'email' => 'engrabdurrahman4991@gmail.com',
-        //     'user_type'=>'ADMIN',
-        //     'password'=>bcrypt('123456789'),
-
-        // ]);
+        
         User::create([
-            'first_name' => 'Abdur',
-            'last_name'=> 'Rahman',
-            'email' => 'engrabdurrahman4991@gmail.com',
+            'first_name' => 'Admin',
+            'last_name'=> '',
+            'email' => 'admin@gmail.com',
             'password'=>Hash::make('123456789'),
             'user_type'=>'ADMIN',
             'verify_email'=> Carbon::now(),
